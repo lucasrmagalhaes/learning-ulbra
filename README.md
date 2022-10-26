ba18bd459a5e8e02dca26c35d44482314af818e4
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-10-25 21:48:46 -0300

    react-hello

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# ULBRA
+# Atividade Prática Avaliativa
+- Utilizando a ferramenta React ou o Angular você deve desenvolver uma aplicação de teste (pode ser algo simples, como um "Hello, World", por exemplo). Você pode usar a sua criatividade para desenvolver uma aplicação similar a essa.
+Deve ser feita a entrega de um print de tela do resultado obtido e dos arquivos compactados (.zip, por exemplo) (ou do link do GitHub).
 
-- Atividades e exemplos.
+**Inicializando o projeto**
+```
+npx create-react-app .
+```
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://www.ulbra.br/themes/img/unidade/pre/50anos.png" className="App-logo" alt="logo" />
+
+        <a
+          className="App-link"
+          href="https://www.ulbra.br/themes/img/unidade/pre/50anos.png"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Atividade Prática Avaliativa - Lucas Magalhães
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
